7d471a7b2e7464b71187ec4b2c4ff80b8864aee9
Trung Nguyen <actcms.com@gmail.com>
2025-04-22 12:48:24 +0700

    Initial commit

---
+++ resources/lang/vi/install.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    "welcome" => "Chào mừng",
+    "welcome_title" => "Chào mừng đến với ACTSMS",
+"admin_setup" => "Cài đặt Admin",
+"done_msg" => "Cài đặt xong. Bây giờ bạn có thể đăng nhập.",
+"email" => "Email",
+"password" => "Mật khẩu",
+"password_confirmation" => "Xác nhận mật khẩu",
+'with_demo_data' => 'Cài đặt với dữ liệu demo',
+"welcome_description" => "Cảm ơn bạn đã chọn ACTSMS để quản lý trường học trực tuyến. Vui lòng làm theo các bước để hoàn tất cài đặt ACTSMS!",
+);
